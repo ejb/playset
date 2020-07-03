980d0fc4afee056080c079a74887606479c32375
ejb <mail@elliotbentley.com>
2020-07-03 10:46:16 -0400

    Blank template

---
+++ src/__tests__/game.test.js
@@ -0,0 +1,52 @@
+import { testSprite } from "@replay/test";
+import { mapInputCoordinates } from "@replay/web";
+import { Game, gameProps } from "..";
+
+test("gameplay", () => {
+  const initInputs = {
+    pointer: {
+      pressed: false,
+      justPressed: false,
+      justReleased: false,
+      x: 0,
+      y: 0,
+    },
+  };
+
+  const {
+    nextFrame,
+    jumpToFrame,
+    updateInputs,
+    getTexture,
+    audio,
+  } = testSprite(Game(gameProps), gameProps, {
+    initInputs,
+    mapInputCoordinates,
+  });
+
+  expect(getTexture("icon").props.x).toBe(0);
+  expect(getTexture("icon").props.y).toBe(0);
+  expect(getTexture("icon").props.rotation).toBe(0);
+
+  updateInputs({
+    pointer: {
+      pressed: true,
+      justPressed: true,
+      justReleased: false,
+      x: 100,
+      y: 100,
+    },
+  });
+
+  nextFrame();
+
+  updateInputs(initInputs);
+
+  expect(audio.play).toBeCalledWith("boop.wav");
+
+  jumpToFrame(() => getTexture("icon").props.x > 99.99);
+
+  expect(getTexture("icon").props.y).toBeCloseTo(100);
+
+  expect(audio.play).toHaveBeenCalledTimes(1);
+});
