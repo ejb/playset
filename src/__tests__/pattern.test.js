a2a6bf377fe37a93eb374b5fdaf267b07ed19ceb
ejb <mail@elliotbentley.com>
2020-07-04 14:06:42 -0400

    Pattern test

---
+++ src/__tests__/pattern.test.js
@@ -0,0 +1,26 @@
+import { testSprite } from '@replay/test';
+import { mapInputCoordinates } from '@replay/web';
+import { Pattern } from '../pattern';
+import { gameProps } from '..';
+
+test('Pattern basic init', () => {
+  
+  const pathProps = {
+    id: 'background-path',
+    x: 0,
+    y: 0,
+    width: 150,
+    height: 150,
+    tileWidth: 50,
+    tileHeight: 50,
+    fileName: 'path.png'
+  };
+  
+  const path = testSprite(Pattern(pathProps), gameProps);
+  
+  const textures = path.getTextures();
+  
+  expect(textures.length).toBe(9);
+  expect(textures[0].type).toBe('image');
+  
+});
